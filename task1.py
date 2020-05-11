d75f6b45461ac3085294d7a0a8cf7c2db2ae6ce8
Jai Kumar <jai@dal.ca>
2020-05-11 16:33:00 -0300

    corrected code

--- task1.py
+++ task1.py
@@ -3,12 +3,17 @@ import string
 space = string.whitespace
 punctuations = string.punctuation
 f1 = open('book.txt')
-book = f1.read()
-b1 = book.lower()
-cleanBook = ""
-for char in b1:
-   if char not in punctuations and char not in space:
-       cleanBook = cleanBook + char
-
+book = f1.readlines()
+cleanBook = []
+for line in book:
+  cleanline = ""
+  for char in line:
+    if char not in punctuations and char not in space:
+        cleanline = cleanline + char
+  res = cleanline.lower()
+  if len(res) > 0:
+    cleanBook.append(res)
+  else:
+    pass
 print(cleanBook)
 
